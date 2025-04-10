b6b6935c02584ae89961848ee2d6925566d67350
O'Neal Shockley <shockleyoneal@gmail.com>
2025-04-09 23:20:23 -0700

    added code to ainstance

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -9,3 +9,40 @@ const axiosInstance = axios.create({
     Accept: "application/json",
   },
 });
+
+// Request Interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response Interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // Handle common errors globally
+    if (error.response) {
+      if (error.response.status === 401) {
+        // Redirect to login page
+        window.location.href = "/login";
+      } else if (error.response.status === 500) {
+        console.error("Server error. Please try again later.");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.error("Request timeout. Please try again.");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
