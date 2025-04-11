d98849af84dad5462b117d6465fabc2dbdc431b5
AdrewReyes <andrewreyes.m@gmail.com>
2025-04-11 01:55:36 -0700

    render

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { visualizer } from 'rollup-plugin-visualizer'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [
@@ -42,4 +42,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
