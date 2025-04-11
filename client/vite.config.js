2310f2bee031b7811acea5200fe6b002f1d8b8c9
AdrewReyes <andrewreyes.m@gmail.com>
2025-04-10 23:58:11 -0700

    render fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  chunkSizeWarningLimit: 1000, // Increase chunk size warning limit
+  chunkSizeWarningLimit: 2000, // Increase chunk size warning limit
   build: {
     rollupOptions: {
       output: {
