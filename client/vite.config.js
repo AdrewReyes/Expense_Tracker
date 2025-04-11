e1d94595b17425ff0e692f8d6b3549873730e856
O'Neal Shockley <shockleyoneal@gmail.com>
2025-04-10 17:32:55 -0700

    made changes to frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+  plugins: [react(), tailwindcss()],
+})
